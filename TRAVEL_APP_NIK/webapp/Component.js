1f3e32352fee6a17d09664dad25cd3dce793c6d6
Nikolaus Jochberg <nrjochberg@hotmail.com>
2020-04-17 05:46:29 +0000

    Project first COMMIT, 17/04/2020 by Nik Jochberg, to local repos.

---
+++ TRAVEL_APP_NIK/webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("inst-nik-trial.travel_app_nik.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
